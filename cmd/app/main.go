44b261cc3ed037c882aafb6da414a4911d1e7047
victor <victorbejenar@yandex.ru>
2022-10-25 20:27:27 +0300

    h

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -6,11 +6,10 @@ import (
 	"github.com/victorbej/hack-auth/internal/domain/app"
 	"github.com/victorbej/hack-auth/internal/domain/controllers"
 	"net/http"
-	"os"
 )
 
 func main() {
-	port := os.Getenv("PORT")
+	//port := os.Getenv("PORT")
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
@@ -20,7 +19,7 @@ func main() {
 
 	//router.NotFoundHandler = app.NotFoundHandler
 
-	err := http.ListenAndServe(port, router) //Launch the app, visit localhost:8000/api
+	err := http.ListenAndServe("localhost:8080", router) //Launch the app, visit localhost:8000/api
 	if err != nil {
 		fmt.Print(err)
 	}
