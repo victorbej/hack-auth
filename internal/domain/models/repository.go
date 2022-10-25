a419a402345a9eabd4e96a6bdc3eda30e91291be
victor <victorbejenar@yandex.ru>
2022-10-25 20:12:01 +0300

    del env

--- internal/domain/models/repository.go
+++ internal/domain/models/repository.go
@@ -4,17 +4,16 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/joho/godotenv"
 )
 
 var db *gorm.DB //база данных
 
 func init() {
 
-	e := godotenv.Load() //Загрузить файл .env
-	if e != nil {
-		fmt.Print(e)
-	}
+	//e := godotenv.Load() //Загрузить файл .env
+	//if e != nil {
+	//	fmt.Print(e)
+	//}
 
 	//username := os.Getenv("DB_USER")
 	//password := os.Getenv("DB_PASS")
@@ -23,7 +22,7 @@ func init() {
 
 	//dbUri := fmt.Sprintf("host=%s user=%s dbname=%s password=%s", dbHost, username, dbName, password) //Создать строку подключения
 
-	dbUri := fmt.Sprintf("postgres://mzodgzxneleony:339c90b16d75e73c4001c1cb32d5efa64be126649ab91f33e644d4698e7369b1@ec2-54-227-248-71.compute-1.amazonaws.com:5432/d8f5abl5k4f51n")
+	dbUri := fmt.Sprintf("postgres://jehoyfskadjiwk:c79ff9cab606cc84fbb2b7138257d10abb3937de4faa805962852e11539bae5f@ec2-23-20-140-229.compute-1.amazonaws.com:5432/d6rc58i6vp57i1")
 	fmt.Println(dbUri)
 
 	conn, err := gorm.Open("postgres", dbUri)
