edff408de37baa4346fbc9f4a1ad0223844cc06b
victor <victorbejenar@yandex.ru>
2022-10-30 14:23:26 +0300

    allow

--- internal/domain/controllers/dataControllers.go
+++ internal/domain/controllers/dataControllers.go
@@ -8,6 +8,12 @@ import (
 )
 
 var CreateUserTable = func(w http.ResponseWriter, r *http.Request) {
+	(w).Header().Set("Access-Control-Allow-Origin", "*")
+	(w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(w).Header().Set("Access-Control-Allow-Credentials", "true")
+	(w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	w.Header().Add("Content-Type", "application/json")
 
 	user := r.Context().Value("user").(uint) //Grab the id of the user that send the request
 	ut := &models.UserTable{}
@@ -24,6 +30,13 @@ var CreateUserTable = func(w http.ResponseWriter, r *http.Request) {
 }
 
 var GetUserTablesFor = func(w http.ResponseWriter, r *http.Request) {
+	(w).Header().Set("Access-Control-Allow-Origin", "*")
+	(w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(w).Header().Set("Access-Control-Allow-Credentials", "true")
+	(w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	w.Header().Add("Content-Type", "application/json")
+
 	id := r.Context().Value("user").(uint)
 	data := models.GetUserTables(id)
 	resp := utils.Message(true, "success")
