de64e2512cf50e159a99fb73cd0d96ece54593e2
victor <victorbejenar@yandex.ru>
2022-11-01 12:54:58 +0300

    return cors

--- internal/domain/utils/utils.go
+++ internal/domain/utils/utils.go
@@ -10,10 +10,10 @@ func Message(status bool, message string) map[string]interface{} {
 }
 
 func Respond(w http.ResponseWriter, data map[string]interface{}) {
-	//(w).Header().Set("Access-Control-Allow-Origin", "*")
-	//(w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	//(w).Header().Set("Access-Control-Allow-Credentials", "true")
-	//(w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	(w).Header().Set("Access-Control-Allow-Origin", "*")
+	(w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(w).Header().Set("Access-Control-Allow-Credentials", "true")
+	(w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(data)
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
-	"github.com/rs/cors"
 	"github.com/victorbej/hack-auth/internal/domain/app"
 	"github.com/victorbej/hack-auth/internal/domain/controllers"
 	"net/http"
@@ -23,16 +22,7 @@ func main() {
 
 	//router.NotFoundHandler = app.NotFoundHandler
 
-	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"Access-Control-Allow-Origin", "*"},
-		AllowedMethods:   []string{"Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE"},
-		AllowedHeaders:   []string{"Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"},
-		AllowCredentials: true,
-	})
-
-	handler := c.Handler(router)
-
-	err := http.ListenAndServe(":"+port, handler) //Launch the app, visit localhost:8000/api
+	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
 	if err != nil {
 		fmt.Print(err)
 	}
